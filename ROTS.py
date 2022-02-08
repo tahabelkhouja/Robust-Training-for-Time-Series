6abbd581ac21eca81df3e0e039d4f759acb74277
Taha Belkhouja <taha.belkhouja@gmail.com>
2022-02-08 08:55:12 -0800

    GPU Run Complete 3

--- ROTS.py
+++ ROTS.py
@@ -2,7 +2,7 @@ import sys
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = "3"
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
-os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 import json
 
 import tensorflow as tf
